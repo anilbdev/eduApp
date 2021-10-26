cee4958f92536187a083782218138656616299ad
Anil Bhasi <anilbhasib@gmail.com>
2021-10-26 23:35:44 +0530

    message styling done

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 
 export default class Message extends Component {
+    
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={styles.container}>
+                <Text style={styles.textHeading}> Heading </Text>
+                <Text style={styles.textMessage}>Message</Text>
+                <Text style={styles.messagePhoneNumber}>Phone number</Text>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'yellow',
+        alignItems:'center'
+    },
+    textHeading: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical:10
+    },
+    textMessage: {
+        fontSize: 15,
+        color: 'grey',
+    },
+    messagePhoneNumber:{
+        fontSize:18,
+        marginTop:10
+    }
+})
