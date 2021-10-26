1bd085a7a837e71f0500ed3f0ef777d6fc618c5e
Anil Bhasi <anilbhasib@gmail.com>
2021-10-26 18:25:04 +0530

    stack navigation boilerplate-class based

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -8,16 +8,20 @@ import Login from '../screens/Login/Login';
 const Stack = createNativeStackNavigator();
 
 
-const Routes = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Login" component={Login} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+
+export class Routes extends Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="Login" component={Login} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        )
+    }
 }
 
 export default Routes
+
 
