40f7adb54a62ad8b4f01888c9c202b288a463e70
Anil Bhasi <anilbhasib@gmail.com>
2021-10-26 21:24:24 +0530

    message component bolierplate

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View, Image, StyleSheet } from 'react-native'
+import Message from '../../components/Message'
 
 export class Login extends Component {
     render() {
         return (
-            <View style={styles.logoImage}>
-                <Image
-                    source={require('../../../assets/logo.png')} />
+            <View style={styles.conatianer}>
+                <View style={styles.logoImage}>
+                    <Image
+                        source={require('../../../assets/logo.png')} />
+                </View>
+                <Message/>
+                <Text>Phone Numbergffgndlgdgldbgld</Text>
+                <Text>Phone Numbergffgndlgdgldbgld</Text>
             </View>
         )
     }
@@ -15,9 +21,19 @@ export class Login extends Component {
 export default Login
 
 const styles = StyleSheet.create({
+    conatianer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        borderColor:'blue',
+        borderWidth:3
+
+    },
     logoImage: {
         alignItems: 'center',
         justifyContent: 'center',
-        flex: 1
+        flex: 1,
+        borderColor:'blue',
+        borderWidth:3
     }
 })
