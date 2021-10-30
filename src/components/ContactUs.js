9d9263ab22873c6e5e406802d1cc2abc4e90de63
Anil Bhasi <anilbhasib@gmail.com>
2021-10-31 01:01:38 +0530

    OTP timer added

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
-
 export default class ContactUs extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.state={
+            timer:3
+        }
     }
+
+    componentDidMount(){
+        this.interval = setInterval(
+          () => this.setState((prevState)=> ({ timer: prevState.timer - 1 })),
+          1000
+        );
+      }
+    
+      componentDidUpdate(){
+        if(this.state.timer === 0){ 
+          console.log("-------------------timer count down is leaking")
+          clearInterval(this.interval);
+          
+        }
+      }
+    
+     
     render() {
-        const { content,handleButtonPress } = this.props
+      
+        const { content, handleButtonPress } = this.props
         return (
             <View>
-                {content.contactUs ? <Text>Contact Us</Text> : <Text></Text>}
+                {content.contactUs ? <View style={styles.conatiner}>
+                    <Text>Resend otp after {this.state.timer}</Text>
+                    <Text>Contact vsvxvxUs</Text>
+                </View> : <Text></Text>}
             </View>
         )
-        
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    conatiner: {
+        borderWidth: 3,
+        justifyContent: 'space-between'
+    }
+})
