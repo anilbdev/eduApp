97b04b00701e318cf49e27810ba8b2d028a06418
Anil Bhasi <anilbhasib@gmail.com>
2021-11-02 01:28:47 +0530

    Carousal Button added-style pending

--- src/components/CarousalDisplay.js
+++ src/components/CarousalDisplay.js
@@ -1,66 +1,94 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import Carousel from 'react-native-snap-carousel';
-
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 export default class Carousal extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-          activeIndex:0,
-          carouselItems: [
-          {
-              title:"Item 1",
-              text: "Text 1",
-          },
-          {
-              title:"Item 2",
-              text: "Text 2",
-          },
-          {
-              title:"Item 3",
-              text: "Text 3",
-          },
-          {
-              title:"Item 4",
-              text: "Text 4",
-          },
-          {
-              title:"Item 5",
-              text: "Text 5",
-          },
-        ]
-      }
+            activeIndex: 0,
+            buttonName: 'Finish',
+            carouselItems: [
+                {
+                    title: "Item 1",
+                    text: "Text 1",
+                },
+                {
+                    title: "Item 2",
+                    text: "Text 2",
+                },
+                {
+                    title: "Item 3",
+                    text: "Text 3",
+                },
+                {
+                    title: "Item 4",
+                    text: "Text 4",
+                },
+                {
+                    title: "Item 5",
+                    text: "Text 5",
+                },
+            ]
+        }
     }
-    _renderItem({item,index}){
+    _renderItem({ item, index }) {
         return (
-          <View style={{
-              backgroundColor:'floralwhite',
-              borderRadius: 5,
-              height: '60%',
-              padding: 50,
-              marginLeft: 25,
-              marginRight: 25, }}>
-            <Text style={{fontSize: 30}}>{item.title}</Text>
-            <Text>{item.text}</Text>
-          </View>
-
+            <View style={styles.itemContainer}>
+                <Text style={{ fontSize: 30 }}>{item.title}</Text>
+                <Text>{item.text}</Text>
+            </View>
         )
     }
     render() {
+        const content = {
+            buttonName: 'Finish'
+        }
         return (
             <View>
-                   <Carousel
-                  layout={"default"}
-                  ref={ref => this.carousel = ref}
-                  data={this.state.carouselItems}
-                  sliderWidth={300}
-                  itemWidth={300}
-                  renderItem={this._renderItem}
-                  onSnapToItem = { index => this.setState({activeIndex:index}) } />
+                <View style={styles.container}>
+                    <Carousel
+                        layout={"default"}
+                        ref={ref => this.carousel = ref}
+                        data={this.state.carouselItems}
+                        sliderWidth={350}
+                        itemWidth={300}
+                        renderItem={this._renderItem}
+                        onSnapToItem={index => this.setState({ activeIndex: index })} />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        icon={
+                            <Icon
+                                name="arrow-right"
+                                size={15}
+                                color="white"
+                            />
+                        }
+                        containerStyle={{ width: 100, height: 50 }}
+                    />
+                </View>
             </View>
-            
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 3,
+        paddingTop: 20
+    },
+    itemContainer: {
+        backgroundColor: '#bfa45a',
+        borderRadius: 15,
+        height: '90%',
+        padding: 0,
+        marginLeft: 0,
+        marginRight: 0,
+    },
+    buttonContainer:{
+        justifyContent:'center',
+        alignItems:'center'     
+        
+    }
+})
