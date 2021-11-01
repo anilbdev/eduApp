cba1acbf807e0eadc89675f9f0a9ecfc143008c6
Anil Bhasi <anilbhasib@gmail.com>
2021-11-01 17:35:57 +0530

    Picker working no styling done

--- src/components/PickerInput.js
+++ src/components/PickerInput.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-import {Picker} from '@react-native-picker/picker';
+import { Text, StyleSheet, View} from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+
 
 
 export default class PickerInput extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            selectedLanguage:''
+        }
+    }
     render() {
         return (
             <View>
-                <Text> This is picker </Text>
+               <Picker
+               selectedValue={this.state.selectedLanguage}
+               onValueChange={(itemValue,itemIndex)=>{
+                   this.setState({
+                       selectedLanguage:itemValue
+                   })
+               }}
+               >
+                   <Picker.Item label="Anil" value="Its value" />
+                   <Picker.Item label="Bhasi" value="My second name" />
+               </Picker>
             </View>
         )
     }
