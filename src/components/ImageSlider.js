1cd49ffd6eb8b04c23c3dfe3944fc2cb4bc40f2d
Anil <anilbahsib@gmail.com>
2021-11-04 15:57:40 +0530

    scroll view added

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image, ScrollView } from 'react-native'
+
+const images = [
+    'https://images.pexels.com/photos/861126/pexels-photo-861126.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/815653/pexels-photo-815653.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/947644/pexels-photo-947644.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/2261480/pexels-photo-2261480.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+]
 
 export default class ImageSlider extends Component {
     render() {
         return (
-            <View>
-                <Text> Image slider </Text>
+            <View style={styles.imageContainer}>
+                <ScrollView>
+                    {
+                        images.map((image, index) => (
+                            <Image
+                                key={index}
+                                source={{ uri: image }}
+                                style={styles.imageStyle}
+                            />
+                        ))
+                    }
+                </ScrollView>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'contain'
+    },
+    imageContainer: {
+        marginTop: 10
+    }
+})
