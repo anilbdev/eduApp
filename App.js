3212a66cf5e81723cc3353fb6bc1b261745369da
Anil Bhasi <anilbhasib@gmail.com>
2021-11-05 09:21:04 +0530

    Item botten border done

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+
 import Routes from './src/navigation/Routes'
-import AppTour from './src/screens/AppTour'
+import { ItemButton } from './src/index';
+
 
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <Routes/>
-     {/* <AppTour/> */}
+      {/* <Routes/> */}
+      <ItemButton/>
     </SafeAreaProvider>
   )
 }
--- src/components/Home/ItemButton.js
+++ src/components/Home/ItemButton.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, TouchableHighlight } from 'react-native'
 export default class ItemButton extends Component {
     render() {
         return (
-            <View>
-                <Text> Item Button </Text>
+            <View style={styles.container} >
+                <TouchableHighlight style={styles.itemButtonStyle}>
+                    
+                    <Text>helloo</Text>
+                </TouchableHighlight>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        width: '35%',
+        borderColor:'grey',
+        borderWidth:3,
+        borderRadius:10
+    },
+    itemButtonStyle: {
+        height: 50,
+        flexDirection: 'row',        
+        alignItems: "center",
+        width: '100%',
+        alignSelf: "center",
+        padding: 14,
+        borderRadius: 10
+    }
+})
