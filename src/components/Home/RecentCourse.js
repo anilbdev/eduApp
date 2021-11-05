d7ee17071c3a93947e89fe53a837ca7da194ee63
Anil Bhasi <anilbhasib@gmail.com>
2021-11-05 17:57:57 +0530

    Image added making recent course

--- src/components/Home/RecentCourse.js
+++ src/components/Home/RecentCourse.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, Image } from 'react-native'
+//component//
 export default class RecentCourse extends Component {
     render() {
         return (
-            <View>
-                <Text> Recent Course </Text>
+            <View style={styles.container}>
+                <Image
+                    style={styles.imageStyle}
+                    source={{ uri: 'https://images.pexels.com/photos/9025296/pexels-photo-9025296.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' }}
+                />
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+    },
+    imageStyle: {
+        height: 200,
+        width: '100%'
+    }
+})
