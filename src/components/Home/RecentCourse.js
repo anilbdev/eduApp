842a27c4086581e0dff3cc431d3b14dab36e0a59
Anil Bhasi <anilbhasib@gmail.com>
2021-11-05 21:48:11 +0530

    recent video BP done

--- src/components/Home/RecentCourse.js
+++ src/components/Home/RecentCourse.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, Image } from 'react-native'
+import { Text, StyleSheet, View,ImageBackground } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 //component//
 export default class RecentCourse extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image
+                <ImageBackground
                     style={styles.imageStyle}
                     source={{ uri: 'https://images.pexels.com/photos/9025296/pexels-photo-9025296.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' }}
-                />
+                >
+                    <View style={styles.imageTextContainer}>
+                        <Icon
+                        name='circle'
+                        size={25}
+                        />
+                        <Text style={styles.imageText}>Hello</Text>
+                    </View>
+                </ImageBackground>
             </View>
         )
     }
@@ -18,6 +27,19 @@ const styles = StyleSheet.create({
     },
     imageStyle: {
         height: 200,
-        width: '100%'
+        width: '100%',
+        justifyContent:'flex-end'
+    },
+    imageTextContainer:{
+       flexDirection:'row',
+       borderWidth:3,
+       backgroundColor:'orange' ,
+       margin:15,
+       fontSize:25
+    },
+    imageText:{
+        fontSize:25,
+        marginLeft:10,
+        color:'white'
     }
 })
