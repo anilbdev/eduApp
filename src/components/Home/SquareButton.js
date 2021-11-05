12a2ed323ef6ff450fece920a0a873c8a752d98d
Anil Bhasi <anilbhasib@gmail.com>
2021-11-05 10:33:12 +0530

    square button done

--- src/components/Home/SquareButton.js
+++ src/components/Home/SquareButton.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View,TouchableHighlight} from 'react-native'
+import { Icon } from 'react-native-elements'
 export default class SquareButton extends Component {
     render() {
+        const isActive=true
         return (
-            <View>
-                <Text> Square Button </Text>
+            <View style={styles.container} >
+                <TouchableHighlight style={styles.itemButtonStyle}>
+                    <View style={{ flexDirection: 'row', alignItems: "center", }}>
+                        <Icon
+                            name='widgets'
+                            color='green'
+                            size={30}
+                        />
+                        
+                    </View>
+                </TouchableHighlight>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        alignSelf:'flex-start',
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    itemButtonStyle: {
+        
+        alignItems: "center",
+        width: '100%',
+        alignSelf: "center",
+        padding: 5,
+        borderRadius: 10,
+    }
+})
