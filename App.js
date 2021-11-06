5ba356a49c3175fec9c241b4dd6c493d6b617f80
Anil Bhasi <anilbhasib@gmail.com>
2021-11-06 09:46:50 +0530

    recent courses done

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import { ScrollView } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-
-
 import Routes from './src/navigation/Routes'
-import { Home,RecentCourse } from './src/index';
-
-
-
+import { Home, RecentCourse } from './src/index';
+import TestClass from './src/test/TestClass'
 const App = () => {
   return (
     <SafeAreaProvider>
-      <ScrollView>
-        {/* <Routes/> */}
-        {/* <Home /> */}
-        <RecentCourse/>
-      </ScrollView>
+      {/* <Routes/> */}
+      <Home />
+      {/* <RecentCourse/> */}
     </SafeAreaProvider>
   )
 }
--- src/test/TestClass.js
+++ src/test/TestClass.js
@@ -1,122 +1,79 @@
-  
-import React, {useState, useRef} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  StatusBar,
-  TouchableOpacity,
-  Text,
-} from 'react-native';
-import PhoneInput from 'react-native-phone-number-input';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const TestClass = () => {
-  const [value, setValue] = useState('');
-  const [countryCode, setCountryCode] = useState('');
-  const [formattedValue, setFormattedValue] = useState('');
-  const [valid, setValid] = useState(false);
-  const [disabled, setDisabled] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
-  const phoneInput = useRef<PhoneInput>(null);
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, ImageBackground, FlatList, Alert, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+const data = [
+  { id: '1', url: 'https://images.pexels.com/photos/9025296/pexels-photo-9025296.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' },
+  { id: '2', url: 'https://images.pexels.com/photos/9651391/pexels-photo-9651391.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260' },
+  { id: '3', url: 'https://images.pexels.com/photos/3750270/pexels-photo-3750270.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+  { id: '4', url: 'https://images.pexels.com/photos/3989737/pexels-photo-3989737.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' },
+  { id: '5', url: 'https://images.pexels.com/photos/70741/cereals-field-ripe-poppy-70741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
+]
+renderSeparator = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        
-          {showMessage && (
-            <View style={styles.message}>
-              <Text>Country Code : {countryCode}</Text>
-              <Text>Value : {value}</Text>
-              <Text>Formatted Value : {formattedValue}</Text>
-              <Text>Valid : {valid ? 'true' : 'false'}</Text>
-            </View>
-          )}
-          <PhoneInput
-            ref={phoneInput}
-            defaultValue={value}
-            defaultCode="IN"
-            layout="first"
-            onChangeText={(text) => {
-              setValue(text);
-            }}
-            onChangeFormattedText={(text) => {
-              setFormattedValue(text);
-              setCountryCode(phoneInput.current?.getCountryCode() || '');
-            }}
-            countryPickerProps={{withAlphaFilter:true}}
-            disabled={disabled}
-            withDarkTheme
-            withShadow
-            autoFocus
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              const checkValid = phoneInput.current?.isValidNumber(value);
-              setShowMessage(true);
-              setValid(checkValid ? checkValid : false);
-              setCountryCode(phoneInput.current?.getCountryCode() || '');
-              let getNumberAfterPossiblyEliminatingZero = phoneInput.current?.getNumberAfterPossiblyEliminatingZero();
-              console.log(getNumberAfterPossiblyEliminatingZero);
-            }}>
-            <Text style={styles.buttonText}>Check</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.button, disabled ? {} : styles.redColor]}
-            onPress={() => {
-              setDisabled(!disabled);
-            }}>
-            <Text style={styles.buttonText}>{disabled ? 'Activate' : 'Disable'}</Text>
-          </TouchableOpacity>
-        
-      </View>
-    </>
+    <View
+      style={{
+        width: 50,
+        backgroundColor: "#orange",
+      }}
+    />
   );
 };
-
+renderItem = ({ item }) => {
+  console.log(item.url);
+  return (
+    <View style={styles.imageContainer}>
+      <ImageBackground
+        style={styles.imageStyle}
+        source={{ uri: item.url }} >
+        <View style={styles.imageTextContainer}>
+          <Icon
+            name='circle'
+            style={{ color: 'white' }}
+            size={25}
+          />
+          <Text style={styles.imageText}>Hello</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+export default class RecentCourse extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headingText}>Recent Course</Text>
+        <FlatList
+          horizontal
+          data={data}
+          renderItem={renderItem}
+          ItemSeparatorComponent={renderSeparator}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: Colors.lighter,
   },
-  wrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  headingText:{
+    margin:2
   },
-  button: {
-    marginTop: 20,
-    height: 50,
-    width: 300,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#7CDB8A',
-    shadowColor: 'rgba(0,0,0,0.4)',
-    shadowOffset: {
-      width: 1,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-    elevation: 10,
+  imageContainer: {
   },
-  buttonText:{
-    color: 'white',
-    fontSize: 14,
+  imageStyle: {
+    height: 150,
+    width: 200,
+    justifyContent: 'flex-end',
   },
-  redColor: {
-    backgroundColor: '#F57777'
+  imageTextContainer: {
+    flexDirection: 'row',
+    margin: 15,
+    fontSize: 25
   },
-  message: {
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 20,
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-});
-
-export default TestClass;
-
+  imageText: {
+    fontSize: 25,
+    marginLeft: 10,
+    color: 'white'
+  }
+})
