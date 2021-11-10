57ac9b52c5f2a504fd2534ec0f28900066872b8a
Anil Bhasi <anilbhasib@gmail.com>
2021-11-10 19:26:40 +0530

    header BP done

--- src/screens/SubjectScreen.js
+++ src/screens/SubjectScreen.js
@@ -1,14 +1,66 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, TouchableHighlight } from 'react-native'
+import { Icon } from 'react-native-elements'
 export default class SubjectScreen extends Component {
     render() {
         return (
-            <View>
-                <Text> Subject Screen </Text>
+            <View style={styles.container}>
+                <View style={styles.headerContainer}>
+                    <View style={styles.backButtonContainer}>
+                        <TouchableHighlight>
+                            <Icon
+                                name='circle'
+                            />
+                        </TouchableHighlight>
+                    </View>
+                    <View style={styles.subjectNameContainer}>
+                        <Text style={{ fontSize: 28 }} >Biology</Text>
+                        <View style={styles.subjectDetailContainer} >
+                            <View style={styles.chapterSummary}>
+                                <Icon
+                                    name='circle'
+                                />
+                                <Text>Chapters</Text>
+                            </View>
+                            <View style={styles.hourSummary}>
+                                <Icon
+                                    name='circle'
+                                />
+                                 <Text>Hours</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.mainBodyContainer}>
+                </View>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderWidth: 3
+    },
+    headerContainer: {
+        borderWidth: 3
+    },
+    mainBodyContainer: {
+        borderWidth: 2
+    },
+    backButtonContainer: {
+        borderWidth: 5,
+        flexDirection: 'row'
+    },
+    subjectNameContainer: {
+    },
+    subjectDetailContainer: {
+        flexDirection: 'row'
+    },
+    chapterSummary: {
+        flexDirection: 'row'
+    },
+    hourSummary: {
+        flexDirection: 'row'
+    }
+})
