ad3e1ea3f098eacf47230ec4bf0695c23400bac8
Anil Bhasi <anilbhasib@gmail.com>
2021-11-11 08:26:57 +0530

    Subject header done

--- src/screens/SubjectScreen.js
+++ src/screens/SubjectScreen.js
@@ -5,40 +5,40 @@ const data = [{
     id: 1,
     title: 'Chapter 1',
     part: 2,
-    hour:3
+    hour: 3
 },
 {
     id: 2,
     title: 'Chapter 2',
     part: 2,
-    hour:2
+    hour: 2
 },
 {
     id: 3,
     title: 'Chapter 3',
     part: 3,
-    hour:5
+    hour: 5
 },
 {
     id: 4,
     title: 'Chapter 4',
     part: 8,
-    hour:8
+    hour: 8
 },
 {
     id: 5,
     title: 'Chapter 5',
     part: 5,
-    hour:6
+    hour: 6
 },
 {
     id: 6,
     title: 'Chapter 6',
     part: 4,
-    hour:7
+    hour: 7
 }]
 const renderItem = ({ item }) => (
-    <View  style={styles.cardContainer}>
+    <View style={styles.cardContainer}>
         <Text style={{ fontSize: 23 }} >{item.title}</Text>
         <View style={styles.cardDetail} >
             <View style={styles.cardChapterSummary}>
@@ -46,14 +46,14 @@ const renderItem = ({ item }) => (
                     size={17}
                     name='radio-button-checked'
                 />
-                <Text style={{fontSize:17,marginLeft:5}} >{item.part} Chapters</Text>
+                <Text style={{ fontSize: 13, marginLeft: 5 }} >{item.part} Chapters</Text>
             </View>
             <View style={styles.cardHourSummary}>
                 <Icon
                     size={17}
                     name='radio-button-checked'
                 />
-                <Text style={{fontSize:17,marginLeft:5}}>{item.hour} Hours</Text>
+                <Text style={{ fontSize: 13, marginLeft: 5 }}>{item.hour} Hours</Text>
             </View>
         </View>
     </View>
@@ -71,19 +71,23 @@ export default class SubjectScreen extends Component {
                         </TouchableHighlight>
                     </View>
                     <View style={styles.subjectNameContainer}>
-                        <Text style={{ fontSize: 28 }} >Biology</Text>
+                        <Text style={{ fontSize: 32, color: '#fff',marginBottom:5 }} >Biology</Text>
                         <View style={styles.subjectDetailContainer} >
                             <View style={styles.chapterSummary}>
                                 <Icon
+                                    color='green'
+                                    size={17}
                                     name='circle'
                                 />
-                                <Text>Chapters</Text>
+                                <Text style={{ fontSize: 13, marginLeft: 5, color: 'green' }} >Chapters</Text>
                             </View>
                             <View style={styles.hourSummary}>
                                 <Icon
+                                    color='green'
+                                    size={17}
                                     name='circle'
                                 />
-                                <Text>Hours</Text>
+                                <Text style={{ fontSize: 13, marginLeft: 5, color: 'green' }} >Hours</Text>
                             </View>
                         </View>
                     </View>
@@ -105,7 +109,8 @@ const styles = StyleSheet.create({
         borderWidth: 3
     },
     headerContainer: {
-        borderWidth: 3
+        borderWidth: 3,
+        backgroundColor: 'grey'
     },
     mainBodyContainer: {
         borderWidth: 2
@@ -115,43 +120,45 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     subjectNameContainer: {
+        marginHorizontal: 30,
+        paddingVertical:40
     },
     subjectDetailContainer: {
         flexDirection: 'row'
     },
     chapterSummary: {
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems: 'center'
     },
     hourSummary: {
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems: 'center',
+        marginLeft: 15
     },
     cardContainer: {
-       
-        marginHorizontal:30,
-        marginBottom:25,
-        padding:10,
-        backgroundColor:'#fff',
+        marginHorizontal: 30,
+        marginBottom: 25,
+        padding: 10,
+        backgroundColor: '#fff',
         shadowColor: "#000000",
-        elevation:5,
+        elevation: 5,
         shadowOpacity: 0.8,
         shadowRadius: 2,
         shadowOffset: {
-          height: 1,
-          width: 1
+            height: 1,
+            width: 1
         }
     },
-    cardDetail:{
+    cardDetail: {
         flexDirection: 'row'
     },
     cardChapterSummary: {
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems: 'center'
     },
     cardHourSummary: {
         flexDirection: 'row',
-        alignItems:'center',
-        marginLeft:15
+        alignItems: 'center',
+        marginLeft: 15
     },
 })
