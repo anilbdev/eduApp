cc979cf698a394b182ecc1189b798ead067e9d18
Anil Bhasi <anilbhasib@gmail.com>
2021-11-12 15:30:26 +0530

    content and contact BP created

--- src/screens/ChapterTab/VideoDetail/VideoDetail.js
+++ src/screens/ChapterTab/VideoDetail/VideoDetail.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
-
 export default class VideoDetail extends Component {
     render() {
         return (
-            <View>
-                <Text> Video detail screen </Text>
+            <View style={styles.container}>
+                <View style={styles.videoSection}>
+                    <Text>Content</Text>
+                </View>
+                <View style={styles.contactSection}>
+                    <Text>Conatct</Text>
+                </View>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    videoSection: {
+        borderWidth: 2
+    },
+    contactSection: {
+        borderWidth: 2
+    }
+})
